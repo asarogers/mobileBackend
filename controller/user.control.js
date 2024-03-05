1779a15c3fb2fc1e784460a131cd935ff872171e
asa <cyberasasoftware@gmail.com>
2024-03-05 11:29:14 -0600

    debugging

--- controller/user.control.js
+++ controller/user.control.js
@@ -18,20 +18,22 @@ exports.login = async (req, res, next) => {
     try {
         const { email, password } = req.body;
         const user = await UserService.checkUser(email);
-
+        
         if (!user) {
             return res.status(401).json({ status: 401, message: "User doesn't exist" });
         }
 
-
+        console.log("user exists")
         const isMatch = await user.comparePassword(password);
         if (!isMatch) {
+            console.log("password does not match")
             return res.status(401).json({ status: 401, message: "Invalid password" });
         }
-
+        console.log("password matches")
         let tokenData = { _id: user._id, email: user.email };
         const token = await UserService.generateToken(tokenData, "secretKey", "1h");
-
+        
+        console.log("returning to user")
         res.status(200).json({ status: 200, token: token });
     } catch (error) {
         console.error(error);
